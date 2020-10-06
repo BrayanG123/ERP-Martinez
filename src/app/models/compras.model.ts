3bdba6eda12b8351478c8a851e454e0a63f4c7cf
Miguel Angel Cayo <miguelcayocarvajal37@gmail.com>
2020-10-06 12:55:49 -0400

    adasd

--- src/app/models/compras.model.ts
+++ src/app/models/compras.model.ts
@@ -1,13 +1,11 @@
-import { Compraprod } from './compraprod.model';
+import { Compraprod } from "./compraprod.model";
 
 export class Compra {
-
-    constructor (
-        public provider_id: number,
-        public deposit_id: number,
-        public productos: Compraprod[],
-        public id?: Number,
-        products?: string;
-    ) { }
-
+  constructor(
+    public provider_id: number,
+    public deposit_id: number,
+    public productos: Compraprod[],
+    public id?: Number,
+    products?: string
+  ) {}
 }
